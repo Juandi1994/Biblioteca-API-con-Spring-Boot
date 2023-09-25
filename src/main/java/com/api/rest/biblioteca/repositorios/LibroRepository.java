9ff0aa3849a76e7501b2411fe10a0417cc2daca2
Juan Botero <juandiegoboterorincon1994@gmail.com>
2023-09-25 16:10:58 -0500

    Biblioteca API con Spring Boot

---
+++ src/main/java/com/api/rest/biblioteca/repositorios/LibroRepository.java
@@ -0,0 +1,7 @@
+package com.api.rest.biblioteca.repositorios;
+
+import com.api.rest.biblioteca.entidades.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+}
